88664452271b266587b2c631f2809216f16fee28
Butchi Peddi <58072398+butchi-conviva@users.noreply.github.com>
2025-03-27 23:12:53 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,11 +22,5 @@ let package = Package(
             name: "ConvivaAppAnalytics",
             url: "https://github.com/Conviva/conviva-ios-appanalytics/raw/1.0.3/Framework/ConvivaAppAnalytics.xcframework.zip",
             checksum: "84628571af35547e026c62ae5fa2e4a8481d3daef27480293c815862f3f17c90"),
-            linkerSettings: [
-                .linkedFramework("UIKit", .when(platforms: [.iOS,.tvOS])),
-                .linkedFramework("Foundation", .when(platforms: [.iOS,.tvOS])),
-                .linkedFramework("CoreTelephony", .when(platforms: [.iOS])),
-                .unsafeFlags(["-ObjC"], .when(platforms: [.iOS,.tvOS])),
-            ]
     ]
 )
